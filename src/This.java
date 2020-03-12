9e62dcf301e615a4dc61673fdb1830b87802e37b
Anton Vinokurov <anton.vinokurov@lime-systems.com>
2020-03-12 17:57:11 +0200

    static

--- src/This.java
+++ src/This.java
@@ -1,17 +1,28 @@
 public class This {
   public static void main(String[] args) {
-    Human human = new Human();
+    Human human1 = new Human();
+    Human human2 = new Human("Name");
+    Human human3 = new Human("name", 2);
+    System.out.println(Human.getDescription());
   }
 }
 
 class Human{
   String name;
   int age;
+  public static String description = "Nice";
 
   public Human(){
-
+    System.out.println("Hello from first ");
   }
-
+  public Human(String name){
+    this.name = name;
+    System.out.println("Hello from second ");
+  }
+  public Human(String name, int age){
+    this.age = age;
+    System.out.println("Hello from third ");
+  }
   public void setName(String name){
     this.name = name;
   }
@@ -24,6 +35,10 @@ class Human{
   }
   public int getAge(){
     return age;
+  }
+
+  public static String getDescription(){
+    return description;
   }
 
   public void getInfo(){
